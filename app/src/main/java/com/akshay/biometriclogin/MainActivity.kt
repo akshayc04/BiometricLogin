5eac252340b3d613c4d3004990bbc8e3cc9938cb
Akshay Chougule <akshay.chougule@mobilefrontiers.com>
2025-04-30 22:17:07 -0400

    upgrade AGP and Gradle versions

--- app/src/main/java/com/akshay/biometriclogin/MainActivity.kt
+++ app/src/main/java/com/akshay/biometriclogin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.akshay.biometriclogin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        BiometricLogin.basicLogin(this, "Login","test", onSuccess = {
+            Toast.makeText(this, "Authentication successful!", Toast.LENGTH_SHORT).show()
+        }, onError= { error -> Toast.makeText(this, "Error: $error", Toast.LENGTH_SHORT).show()})
     }
 }
